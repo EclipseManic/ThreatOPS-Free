27e8656d477a8f9732097d7fadce62f19984acb5
EclipseManic <eclipseg12345@gmail.com>
2025-11-08 10:36:32 +0530

    Delete collectors directory

--- collectors/__init__.py
+++
@@ -1,9 +0,0 @@
-# ThreatOps SOC Simulator - Collectors Package
-# Note: Log collection is now handled by Filebeat
-# This module only provides the LogEntry class for compatibility
-
-from .log_collector import LogEntry
-
-__all__ = [
-    'LogEntry'
-]
